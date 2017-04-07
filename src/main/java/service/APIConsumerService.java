4518ee59e9992d1038b9913deee1c4ba5edcfd97
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-04-07 16:50:59 +0530

    Update APIConsumerService.java

--- src/main/java/service/APIConsumerService.java
+++ src/main/java/service/APIConsumerService.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.DateFormat;
+import java.util.Date;
 
 import javax.net.ssl.HttpsURLConnection;
 
