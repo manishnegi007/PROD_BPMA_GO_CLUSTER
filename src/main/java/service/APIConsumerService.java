86e26f5178af39f22917fb9aed34eace6cd82915
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-04-14 11:41:40 +0530

    Update APIConsumerService.java

--- src/main/java/service/APIConsumerService.java
+++ src/main/java/service/APIConsumerService.java
@@ -256,7 +256,7 @@ public class APIConsumerService {
 					fvMap.put("discontinuanceFund",
 							((Map) ((Map) ((Map) resultData.get("response")).get("responseData")).get("BasicDetails"))
 									.get("discontinuanceFund").toString());
-					fvMap.put("Message", resProp.getString("InquiringFVTrue"));
+					fvMap.put("Message", resProp.getString("InquiringFVTrue") + ". " + resProp.getString("InquiringFVTrue_F"));
 					returnMap.put("FV", fvMap);
 				} else if ("8".equals(policyInsuranceTypeCd) || "1".equals(policyInsuranceTypeCd)) {
 					Map<String, String> fvMap = new HashMap();
