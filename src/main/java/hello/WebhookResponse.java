00f9f7c3e9731e0a2fceb3aca44dc075f57d26b6
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-06-12 16:12:14 +0530

    Update WebhookResponse.java

--- src/main/java/hello/WebhookResponse.java
+++ src/main/java/hello/WebhookResponse.java
@@ -1,35 +1,35 @@
 package hello;
 
 public class WebhookResponse {
-    private final String speech;
-    private final String displayText;
-
-    private final String source = "java-webhook";
-    
-    private String speech;
-	 private String displayText;
-	 
-	 
-	 
-	public WebhookResponse() {
-		super();
-	}
+   	  String speech;
+	  String displayText;
+	  
+	  
 	public WebhookResponse(String speech, String displayText) {
 		super();
 		this.speech = speech;
 		this.displayText = displayText;
 	}
+
+
 	public String getSpeech() {
 		return speech;
 	}
+
+
 	public void setSpeech(String speech) {
 		this.speech = speech;
 	}
+
+
 	public String getDisplayText() {
 		return displayText;
 	}
+
+
 	public void setDisplayText(String displayText) {
 		this.displayText = displayText;
 	}
+	 
 	
 }
