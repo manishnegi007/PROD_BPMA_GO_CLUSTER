0d5f00d0bcfd7f2c4b302f1e2ff753bcd8251a80
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-06-12 15:59:52 +0530

    Update APIConsumerService.java

--- src/main/java/service/APIConsumerService.java
+++ src/main/java/service/APIConsumerService.java
@@ -20,7 +20,7 @@ import common.XTrustProvider;
 import hello.WebhookResponse;
 @Component
 public class APIConsumerService {
-	private static Logger logger = LogManager.getLogger(APIConsumerService.class);
+	
 	public WebhookResponse getWipDataAll(String action, String channel, String period, String productType, String planType)
 	{
 		ResourceBundle res = ResourceBundle.getBundle("errorMessages");
