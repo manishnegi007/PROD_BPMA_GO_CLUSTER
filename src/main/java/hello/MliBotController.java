ac3d22220abac86db507dabf0e8313524f066d6e
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-06-14 15:32:55 +0530

    Create MliBotController.java

--- src/main/java/hello/MliBotController.java
+++ src/main/java/hello/MliBotController.java
@@ -1,11 +1,8 @@
 package hello;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.ResourceBundle;
-
 import javax.servlet.http.HttpSession;
-
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -24,6 +21,7 @@ import service.APIConsumerService;
 public class MliBotController{
 
 	public static ResourceBundle resProp = ResourceBundle.getBundle("errorMessages");
+        public static Map<String, Map<String,String>> sessionMap = new HashMap<String, Map<String,String>>();
 	
 	@Autowired
 	APIConsumerService aPIConsumerService;
@@ -37,55 +35,105 @@ public class MliBotController{
 		String planType="";
 		String period="";
 		String channel="";
-		
+		String sessionId="";
+		String cachePeriod=""; String cashplanType=""; String cashchannel=""; String cashproductType="";
+
 		WebhookResponse response = new WebhookResponse();
+/*		MliBotControllerRest mliBotControllerRest= new MliBotControllerRest();*/
 		try 
 		{
 			System.out.println("WebhookResponse API START");
 			JSONObject object = new JSONObject(obj);
 			String actionperformed = object.getJSONObject("result").get("action")+"";
+			sessionId = object.get("sessionId")+"";
+/*			String ssoId="";
+			String password="";
+			switch(actionperformed)
+			{
+			case "SSOValidation":
+			{
+				String status = mliBotControllerRest.APICallSSOValidation(ssoId, password);
+				JSONObject finalStatus = new JSONObject(status);
+			}
+			break;
+			}*/
 			try{
 				channel = object.getJSONObject("result").getJSONObject("parameters").getString("Channel")+"";
 			}catch(Exception e)
 			{
-				
 				channel = "";
 			}
 			try{
 				productType = object.getJSONObject("result").getJSONObject("parameters").getString("ProductType")+"";
 			}catch(Exception e)
 			{
-				
 				productType="";
 			}
 			try{
-				period = object.getJSONObject("result").getJSONArray("contexts").getJSONObject(0).getJSONObject("parameters").getString("Period")+"";
+				period =  object.getJSONObject("result").getJSONObject("parameters").getString("Period")+""; 
 			}catch(Exception e)
 			{
-				
 				period="";
 			}
 			try{
 				planType = object.getJSONObject("result").getJSONObject("parameters").getString("planType")+"";
 			}catch(Exception e)
 			{
-				
 				planType="";
 			}
-			if(!actionperformed.equalsIgnoreCase("") && actionperformed!=null)
+			if(sessionMap.containsKey(sessionId))
 			{
-				return aPIConsumerService.getWipDataAll(actionperformed, channel, period, productType, planType);
+				if(period.equalsIgnoreCase("")){
+					cachePeriod=sessionMap.get(sessionId).get("period")+"";
+				}else
+				{
+					cachePeriod=period;
+				}
+				if(planType.equalsIgnoreCase("")){	
+					cashplanType= sessionMap.get(sessionId).get("planType")+"";
+				}else
+				{
+					cashplanType=planType;
+				}
+				if(channel.equalsIgnoreCase("")){
+					cashchannel= sessionMap.get(sessionId).get("channel")+"";
+				}else{
+					cashchannel=channel;
+				}
+				if(productType.equalsIgnoreCase("")){	
+					cashproductType= sessionMap.get(sessionId).get("productType")+"";
+				}else{
+					cashproductType=productType;
+				}
+				if(!actionperformed.equalsIgnoreCase("") && actionperformed!=null)
+				{
+					response = aPIConsumerService.getWipDataAll(actionperformed, cashchannel, cachePeriod, cashproductType, cashplanType);
+				}
+			}
+			else
+			{
+				Map<String, String> map = new HashMap<String, String>();
+				map.put("channel", channel);
+				map.put("productType", productType);
+				map.put("period", period);
+				map.put("planType", planType);
+				sessionMap.put(sessionId, map);
+				if(!actionperformed.equalsIgnoreCase("") && actionperformed!=null)
+				{
+					response = aPIConsumerService.getWipDataAll(actionperformed, channel, period, productType, planType);
+				}
 			}
 		} 
 		catch (Exception e)
 		{
-			
+		 System.out.println("End : Controller: Webhook- Exception");	
 		}
 		WebhookResponse responseObj = new WebhookResponse(speech, speech);
 		System.out.println("End : Controller: Webhook");
 		return responseObj;
 	}
-
 }
+
+
 
 
