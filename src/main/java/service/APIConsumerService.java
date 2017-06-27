71bd0131534d00c58824bf06be59c92e166d2da6
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-06-27 11:33:03 +0530

    Update APIConsumerService.java

--- src/main/java/service/APIConsumerService.java
+++ src/main/java/service/APIConsumerService.java
@@ -27,6 +27,11 @@ public class APIConsumerService {
 		{
 			channel=channel.toUpperCase();
 		}
+		else
+		{
+			channel=convertToCamelCase(channel);
+
+		}
 		
 		System.out.println("getWipDataAll API START");
 		ResourceBundle res = ResourceBundle.getBundle("errorMessages");
@@ -678,6 +683,22 @@ public class APIConsumerService {
 		return responseObj;
 		
 	}
+public String convertToCamelCase(String channel){
 
+		final String ACTIONABLE_DELIMITERS = " '-/"; // these cause the character following
+		// to be capitalized
+
+		StringBuilder sb = new StringBuilder();
+		boolean capNext = true;
+
+		for (char c : channel.toCharArray()) {
+			c = (capNext)
+					? Character.toUpperCase(c)
+							: Character.toLowerCase(c);
+					sb.append(c);
+					capNext = (ACTIONABLE_DELIMITERS.indexOf((int) c) >= 0); // explicit cast not needed
+
+		}
+		return sb.toString();
+	}
 }
-
