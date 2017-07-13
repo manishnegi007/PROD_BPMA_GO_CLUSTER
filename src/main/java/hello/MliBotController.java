1041f9452e56dc35f7b9e07eb54d49d13dd51284
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-07-13 17:09:02 +0530

    Update MliBotController.java

--- src/main/java/hello/MliBotController.java
+++ src/main/java/hello/MliBotController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import utils.HttpUrlConnectionMlabInsert;
 
 import common.Commons;
 import  service.APIConsumerService;
