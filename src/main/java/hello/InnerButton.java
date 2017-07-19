3c0c6173255bec2d289483d5365e7f5d1b473800
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-07-19 10:28:35 +0530

    Update InnerButton.java

--- src/main/java/hello/InnerButton.java
+++ src/main/java/hello/InnerButton.java
@@ -2,6 +2,10 @@ package hello;
 public class InnerButton 
 {
 	String text;
+	String text2;
+	String text3;
+	String text4;
+	String text5;
 	String postback;
 	
 	public InnerButton() {
@@ -9,11 +13,47 @@ public class InnerButton
 		// TODO Auto-generated constructor stub
 	}
 
-	public InnerButton(String text, String postback) {
+	public InnerButton(String text, String text2, String text3, String text4, String text5, String postback) {
 		super();
 		this.text = text;
+		this.text2 = text2;
+		this.text3 = text3;
+		this.text4 = text4;
+		this.text5 = text5;
 		this.postback = postback;
 	}
+	public String getText2() {
+		return text2;
+	}
+
+	public void setText2(String text2) {
+		this.text2 = text2;
+	}
+
+	public String getText3() {
+		return text3;
+	}
+
+	public void setText3(String text3) {
+		this.text3 = text3;
+	}
+
+	public String getText4() {
+		return text4;
+	}
+
+	public void setText4(String text4) {
+		this.text4 = text4;
+	}
+
+	public String getText5() {
+		return text5;
+	}
+
+	public void setText5(String text5) {
+		this.text5 = text5;
+	}
+
 
 	public String getText() {
 		return text;
