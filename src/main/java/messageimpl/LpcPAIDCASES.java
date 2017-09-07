25748e952a36cadb03fe26d26c61075ea68ee389
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-07 16:14:18 +0530

    Update LpcPAIDCASES.java

--- src/main/java/messageimpl/LpcPAIDCASES.java
+++ src/main/java/messageimpl/LpcPAIDCASES.java
@@ -2,8 +2,10 @@ package messageimpl;
 
 public class LpcPAIDCASES {
 
-	public static String lpcPaidCasesIntent(String channel, String period, String user_region, String user_circle, String userzone , String real_tim_timstamp,
-     String lpc_paid_cases_mtd, String lpc_paid_cases_ytd )
+	public static String lpcPaidCasesIntent(String channel, String period, String user_region,
+     String user_circle, String userzone , String real_tim_timstamp,
+     String lpc_paid_cases_mtd, String lpc_paid_cases_ytd, String subchannel )
+
 	{
 		String finalresponse="";
 		if("MLI".equalsIgnoreCase(channel))
@@ -14,6 +16,11 @@ public class LpcPAIDCASES {
 		{
 			user_region="Circle "+user_circle;
 		}
+		if(!"".equalsIgnoreCase(subchannel))
+	        {
+                 channel = subchannel;
+	        }
+
 		if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= "As of " + real_tim_timstamp + " LPC Paid cases MTD for MLI is  "
