1c7b34c70f60a2e3f6ac810d7c2e027a0006aef3
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-07 08:27:24 +0530

    Create Applied.java

---
+++ src/main/java/messageimpl/Applied.java
@@ -0,0 +1,51 @@
+package messageimpl;
+
+public class Applied 
+{
+	public static String appliedIntent(String period, String source, String msgChannel, String mtdAppliedAFYP, String real_tim_timstamp,
+			String channel, String dailyAppliedAFYP, String ytd_applied_afyp)
+	{
+		String finalresponse="";
+
+		if("MONTHLY".equalsIgnoreCase(period) ||"MTD".equalsIgnoreCase(period) ||"MONTH".equalsIgnoreCase(period))
+		{
+			if("google".equalsIgnoreCase(source))
+			{
+				finalresponse=" Monthly Applied Business for "+msgChannel+
+						" as of now is : "+mtdAppliedAFYP+" Cr "+"";
+			}
+			else
+			{
+				finalresponse="As of "+real_tim_timstamp+" Applied Business AFYP MTD For "+msgChannel+
+						": "+mtdAppliedAFYP+" Cr "+"";
+			}
+		}
+		else if(!"".equalsIgnoreCase(channel))
+		{
+			if("google".equalsIgnoreCase(source))
+			{
+				finalresponse="Current Update for Applied Business of "+msgChannel+" : "
+						+ " For the day is, : " +dailyAppliedAFYP+" Cr"
+						+", For the month is, : " +mtdAppliedAFYP+" Cr"
+						+", For the year is, : " +ytd_applied_afyp+" Cr";
+			}
+			else
+			{
+				finalresponse="As of "+real_tim_timstamp+" Applied AFYP For "+msgChannel+"  is: "
+						+ " FTD : " +dailyAppliedAFYP+" Cr"
+						+", MTD : " +mtdAppliedAFYP+" Cr"
+						+", YTD : " +ytd_applied_afyp+" Cr";
+			}
+
+		}
+		else
+		{
+			finalresponse="As of "+real_tim_timstamp+" Applied AFYP"+
+					"  is: "
+					+ " FTD : " +dailyAppliedAFYP+" Cr"
+					+", MTD : " +mtdAppliedAFYP+" Cr"
+					+", YTD : " +ytd_applied_afyp+" Cr";
+		}
+		return finalresponse.toString();
+	}
+}
