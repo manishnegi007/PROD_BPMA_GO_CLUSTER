bdf518f8e59c992abcfa9607ba1f6ab5064423ab
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-07 09:40:40 +0530

    Update GrowthLPCAPLADJIFYP.java

--- src/main/java/messageimpl/GrowthLPCAPLADJIFYP.java
+++ src/main/java/messageimpl/GrowthLPCAPLADJIFYP.java
@@ -14,7 +14,7 @@ public class GrowthLPCAPLADJIFYP {
 		{channel="";}
 		if("Monthly".equalsIgnoreCase(period))
 		{period="";}
-		if("AXIS".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(user_circle))
+		if(!"".equalsIgnoreCase(user_circle))
 		{
 			user_region="Circle "+user_circle;
 		}
