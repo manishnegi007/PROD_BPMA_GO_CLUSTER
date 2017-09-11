fb5f30cd9e4180f202c44a3bd6dfb9d5553e4342
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-11 18:54:56 +0530

    Update Lpcaplcases.java

--- src/main/java/messageimpl/Lpcaplcases.java
+++ src/main/java/messageimpl/Lpcaplcases.java
@@ -1,106 +1,108 @@
 package messageimpl;
 
-public class Lpcaplcases {
+public class Lpcaplcases 
+{
+	String finalresponse="";
+		if("Agency".equalsIgnoreCase(channel))
+		{
+			if("MLI".equalsIgnoreCase(channel))
+			{channel="";}
+			if("Monthly".equalsIgnoreCase(period))
+			{period="";}
+			else
+			{period=period.toUpperCase();}
+			if(!"".equalsIgnoreCase(user_circle))
+			{user_region="Circle "+user_circle;}
+			if(!"".equalsIgnoreCase(subchannel))
+			{channel = subchannel;}
 
-	public static String lpcAplCasesIntent(String channel, String period, String user_region, String user_circle, String userzone ,
-	String real_tim_timstamp,
-	String lpc_applied_cases_mtd, String lpc_applied_cases_ytd, String subchannel)
-	{
-		String finalresponse="";
-		if("MLI".equalsIgnoreCase(channel))
-		{channel="";}
-		if("Monthly".equalsIgnoreCase(period))
-		{period="";}
-		else
-		{period=period.toUpperCase();}
-		if(!"".equalsIgnoreCase(user_circle))
-		{user_region="Circle "+user_circle;}
-		if(!"".equalsIgnoreCase(subchannel))
-	        {channel = subchannel;}
-
-		if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
-		{
-			finalresponse= "As of " + real_tim_timstamp + " LPC Applied cases MTD for MLI is  "
-					+ lpc_applied_cases_mtd + ". LPC Applied cases YTD for MLI is  " + lpc_applied_cases_ytd + 
-					". If you want to see the channel wise business numbers, please specify";
-		}
-		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
-		{
-			finalresponse= "As of " + real_tim_timstamp + " LPC Applied cases MTD for " + channel +
-					" is " + lpc_applied_cases_mtd + ". LPC Applied cases YTD for " +channel + " is "+ lpc_applied_cases_ytd+
-					". If you want to see the zone/region wise business numbers, please specify";
-		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
-		{
-			finalresponse= "As of " + real_tim_timstamp + " LPC Applied cases MTD for" + userzone + " zone is "
-					+ lpc_applied_cases_mtd + ". LPC Applied cases YTD for " + userzone + " zone is " + lpc_applied_cases_ytd+
-					". If you want to see the region wise business numbers, please specify";
-		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
-		{
-			finalresponse= "As of " + real_tim_timstamp + " LPC Applied cases MTD " + user_region + " is " 
-					+ lpc_applied_cases_mtd + ". LPC Applied cases YTD for " + user_region + " is " + lpc_applied_cases_ytd+ "";
-
-		}
-		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
-		{
-			if("YTD".equalsIgnoreCase(period))
+			if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 			{
-				finalresponse="As of " + real_tim_timstamp + " LPC Applied cases " + period + " for " +channel+ " is " +lpc_applied_cases_ytd+
+				finalresponse= "As of " + real_tim_timstamp + " LPC Applied cases MTD for MLI is  "
+						+ lpc_applied_cases_mtd + ". LPC Applied cases YTD for MLI is  " + lpc_applied_cases_ytd + 
+						". If you want to see the channel wise business numbers, please specify";
+			}
+			else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+			{
+				finalresponse= "As of " + real_tim_timstamp + " LPC Applied cases MTD for " + channel +
+						" is " + lpc_applied_cases_mtd + ". LPC Applied cases YTD for " +channel + " is "+ lpc_applied_cases_ytd+
 						". If you want to see the zone/region wise business numbers, please specify";
 			}
-			else
+			else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 			{
-				finalresponse="As of " + real_tim_timstamp + " LPC Applied cases " + period + " for " +channel+ " is " +lpc_applied_cases_mtd+
-						". If you want to see the zone/region wise business numbers, please specify";	
-			}
-		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
-		{
-			if("YTD".equalsIgnoreCase(period))
-			{
-				finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " +userzone+ " zone is " +lpc_applied_cases_ytd+
+				finalresponse= "As of " + real_tim_timstamp + " LPC Applied cases MTD for" + userzone + " zone is "
+						+ lpc_applied_cases_mtd + ". LPC Applied cases YTD for " + userzone + " zone is " + lpc_applied_cases_ytd+
 						". If you want to see the region wise business numbers, please specify";
 			}
+			else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+			{
+				finalresponse= "As of " + real_tim_timstamp + " LPC Applied cases MTD " + user_region + " is " 
+						+ lpc_applied_cases_mtd + ". LPC Applied cases YTD for " + user_region + " is " + lpc_applied_cases_ytd+ "";
+
+			}
+			else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+			{
+				if("YTD".equalsIgnoreCase(period))
+				{
+					finalresponse="As of " + real_tim_timstamp + " LPC Applied cases " + period + " for " +channel+ " is " +lpc_applied_cases_ytd+
+							". If you want to see the zone/region wise business numbers, please specify";
+				}
+				else
+				{
+					finalresponse="As of " + real_tim_timstamp + " LPC Applied cases " + period + " for " +channel+ " is " +lpc_applied_cases_mtd+
+							". If you want to see the zone/region wise business numbers, please specify";	
+				}
+			}
+			else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+			{
+				if("YTD".equalsIgnoreCase(period))
+				{
+					finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " +userzone+ " zone is " +lpc_applied_cases_ytd+
+							". If you want to see the region wise business numbers, please specify";
+				}
+				else
+				{
+					finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " +userzone+ " zone is " +lpc_applied_cases_mtd+
+							". If you want to see the region wise business numbers, please specify";	
+				}
+			}
+			else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+			{
+				if("YTD".equalsIgnoreCase(period))
+				{
+					finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " + user_region+ " is " +lpc_applied_cases_ytd+ "";
+				}else
+				{
+					finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " + user_region+ " is " +lpc_applied_cases_mtd+ "";	
+				}
+			}
+			else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+			{
+				if("YTD".equalsIgnoreCase(period))
+				{
+					finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " + user_region+ " is " +lpc_applied_cases_ytd+ "";
+				}else
+				{
+					finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " + user_region+ " is " +lpc_applied_cases_mtd+ "";	
+				}
+			}
 			else
 			{
-				finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " +userzone+ " zone is " +lpc_applied_cases_mtd+
-						". If you want to see the region wise business numbers, please specify";	
-			}
-		}
-		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
-		{
-			if("YTD".equalsIgnoreCase(period))
-			{
-				finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " + user_region+ " is " +lpc_applied_cases_ytd+ "";
-			}else
-			{
-				finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " + user_region+ " is " +lpc_applied_cases_mtd+ "";	
-			}
-		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
-		{
-			if("YTD".equalsIgnoreCase(period))
-			{
-				finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " + user_region+ " is " +lpc_applied_cases_ytd+ "";
-			}else
-			{
-				finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for " + user_region+ " is " +lpc_applied_cases_mtd+ "";	
+				if("YTD".equalsIgnoreCase(period))
+				{
+					finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for MLI is " +lpc_applied_cases_ytd+ "" 
+							+ ". If you want to see the channel wise business numbers, please specify";
+				}else
+				{
+					finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for MLI is " +lpc_applied_cases_mtd+ "" 
+							+ ". If you want to see the channel wise business numbers, please specify";	
+				}
 			}
 		}
 		else
 		{
-			if("YTD".equalsIgnoreCase(period))
-			{
-				finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for MLI is " +lpc_applied_cases_ytd+ "" 
-						+ ". If you want to see the channel wise business numbers, please specify";
-			}else
-			{
-				finalresponse="As of " +real_tim_timstamp+ " LPC Applied cases " +period+ " for MLI is " +lpc_applied_cases_mtd+ "" 
-						+ ". If you want to see the channel wise business numbers, please specify";	
-			}
+			finalresponse="Invalid Channel For LPC! Only Agency Accepted";
 		}
-	
 		return finalresponse.toString();
 	}
 }
