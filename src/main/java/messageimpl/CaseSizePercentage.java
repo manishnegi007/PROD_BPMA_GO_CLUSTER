61be90d132a50f365652a2d2a0040e313f90cfec
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-11 16:03:54 +0530

    Update CaseSizePercentage.java

--- src/main/java/messageimpl/CaseSizePercentage.java
+++ src/main/java/messageimpl/CaseSizePercentage.java
@@ -12,37 +12,31 @@ public class CaseSizePercentage {
 		if("Monthly".equalsIgnoreCase(period))
 		{period="";}
 		else
-		{
-			period=period.toUpperCase();
-		}
+		{period=period.toUpperCase();}
 		if(!"".equalsIgnoreCase(user_circle))
-		{
-			user_region="Circle "+user_circle;
-		}
+		{user_region="Circle "+user_circle;}
 		if(!"".equalsIgnoreCase(subchannel))
-		{
-		        channel = subchannel;
-		}
+		{channel = subchannel;}
 
 		if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= "MLI Case Size acheivement MTD: "+achiev_mtd_case_active_mtd+"%, YTD " +achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-					" If you want to see the channel wise business numbers, please specIfy.";
+					". If you want to see the channel wise business numbers, please specIfy.";
 		}
 		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= channel+" channel Case Size acheivement MTD: "+achiev_mtd_case_active_mtd+"%, YTD " +achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-					" If you want to see the zone/region wise business numbers, please specIfy.";
+					". If you want to see the zone/region wise business numbers, please specIfy.";
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= "Zone "+userzone+" Case Size acheivement MTD: "+achiev_mtd_case_active_mtd+"%, YTD " +achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-					" If you want to see the region wise business numbers, please specIfy.";
+					". If you want to see the region wise business numbers, please specIfy.";
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= "Region "+user_region+" Case Size acheivement MTD: "+achiev_mtd_case_active_mtd+"%, YTD " +achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-					" If you want to see the region wise business numbers, please specIfy.";
+					". If you want to see the region wise business numbers, please specIfy.";
 
 		}
 		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
@@ -51,11 +45,11 @@ public class CaseSizePercentage {
 			if("YTD".equalsIgnoreCase(period))
 			{
 				finalresponse= channel+" channel Case Size acheivement "+period+" : "+achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-						" If you want to see the zone/region wise business numbers please specIfy. ";
+						". If you want to see the zone/region wise business numbers please specIfy. ";
 			}else
 			{
 				finalresponse= channel+" channel Case Size acheivement "+period+" : "+achiev_mtd_case_active_mtd+"% till "+real_tim_timstamp+
-						" If you want to see the zone/region wise business numbers please specIfy. ";
+						". If you want to see the zone/region wise business numbers please specIfy. ";
 			}
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
@@ -63,11 +57,11 @@ public class CaseSizePercentage {
 			if("YTD".equalsIgnoreCase(period))
 			{
 				finalresponse= "Zone "+userzone+" Case Size acheivement "+period+" : "+achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-						" If you want to see the region wise business numbers please specIfy";
+						". If you want to see the region wise business numbers please specIfy";
 			}else
 			{
 				finalresponse= "Zone "+userzone+" Case Size acheivement "+period+" : "+achiev_mtd_case_active_mtd+"% till "+real_tim_timstamp+
-						" If you want to see the region wise business numbers please specIfy";
+						". If you want to see the region wise business numbers please specIfy";
 			}
 		}
 		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
@@ -75,11 +69,11 @@ public class CaseSizePercentage {
 			if("YTD".equalsIgnoreCase(period))
 			{
 				finalresponse= "Region "+user_region+" Case Size acheivement " +period+" : "+achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-						" If you want to see the region wise business numbers please specIfy.";
+						". If you want to see the region wise business numbers please specIfy.";
 			}else
 			{
 				finalresponse= "Region "+user_region+" Case Size acheivement " +period+" : "+achiev_mtd_case_active_mtd+"% till "+real_tim_timstamp+
-						" If you want to see the region wise business numbers please specIfy.";
+						". If you want to see the region wise business numbers please specIfy.";
 			}
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
@@ -87,11 +81,11 @@ public class CaseSizePercentage {
 			if("YTD".equalsIgnoreCase(period))
 			{
 				finalresponse= "Region "+user_region+" Case Size acheivement " +period+" : "+achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-						" If you want to see the region wise business numbers please specIfy.";
+						". If you want to see the region wise business numbers please specIfy.";
 			}else
 			{
 				finalresponse= "Region "+user_region+" Case Size acheivement " +period+" : "+achiev_mtd_case_active_mtd+"% till "+real_tim_timstamp+
-						" If you want to see the region wise business numbers please specIfy.";
+						". If you want to see the region wise business numbers please specIfy.";
 			}
 		}
 		else
@@ -99,11 +93,11 @@ public class CaseSizePercentage {
 			if("YTD".equalsIgnoreCase(period))
 			{
 				finalresponse= "MLI Case Size acheivement " +period+" : "+achiev_mtd_case_active_ytd+"% till "+real_tim_timstamp+
-						" If you want to see the channel wise business numbers please specIfy.";
+						". If you want to see the channel wise business numbers please specIfy.";
 			}else
 			{
 				finalresponse= "MLI Case Size acheivement " +period+" : "+achiev_mtd_case_active_mtd+"% till "+real_tim_timstamp+
-						" If you want to see the channel wise business numbers please specIfy.";
+						". If you want to see the channel wise business numbers please specIfy.";
 			}
 		}
 		return finalresponse.toString();
