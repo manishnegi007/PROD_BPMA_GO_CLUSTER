3aaa40632b2c1eea3f75cecb27630528ebdfbbd8
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-11 13:25:37 +0530

    Update ProductMix.java

--- src/main/java/messageimpl/ProductMix.java
+++ src/main/java/messageimpl/ProductMix.java
@@ -16,6 +16,10 @@ public class ProductMix
 		{channel="";}
 		if("Monthly".equalsIgnoreCase(period))
 		{period="";}
+		else
+		{
+			period=period.toUpperCase();
+		}
 		if(!"".equalsIgnoreCase(user_circle))
 		{
 			user_region="Circle "+user_circle;
@@ -84,6 +88,19 @@ public class ProductMix
 				finalresponse="As of "+real_tim_timstamp+" "+period+" product mix ratio on Adj MFYP for" +userzone+ " zone is ULIP: "+ul_penet_ytd_adj_mfyp+"%, Par: "+par_penet_ytd_adj_mfyp+" % & Non-Par: "+nonpar_penet_ytd_adj_mfyp+"%, protection: "+
 						protec_penet_ytd_adj_mfyp+"%. paid case ratio: ULIP: "+ul_penet_ytd_pol_cnt+"%, par: "+par_penet_ytd_pol_cnt+"%, Non-par: "+nonpar_penet_ytd_pol_cnt+"% & protection: "+protec_penet_ytd_pol_cnt+
 						"% If you want to see the region wise business numbers, please specify";	
+			}
+		}
+		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+		{
+			if("MTD".equalsIgnoreCase(period))
+			{
+				finalresponse="As of "+real_tim_timstamp+" "+period+" product mix ratio on Adj MFYP for" +user_region+ " is ULIP: "+ul_penet_mtd_adj_mfyp+" % , Par: "+par_penet_mtd_adj_mfyp+" % & Non-Par: "+nonpar_penet_mtd_adj_mfyp+" % , protection: "+
+						protec_penet_mtd_adj_mfyp+" %. paid case ratio: ULIP: "+ul_penet_mtd_pol_cnt+" % , par: "+par_penet_mtd_pol_cnt+" % , Non-par: "+nonpar_penet_mtd_pol_cnt+" % & protection: "+protec_penet_mtd_pol_cnt+" %";
+			}
+			else
+			{
+				finalresponse="As of "+real_tim_timstamp+" "+period+" product mix ratio on Adj MFYP for" +user_region+ " is ULIP: "+ul_penet_ytd_adj_mfyp+" % , Par: "+par_penet_ytd_adj_mfyp+" % & Non-Par: "+nonpar_penet_ytd_adj_mfyp+" % , protection: "+
+						protec_penet_ytd_adj_mfyp+" %. paid case ratio: ULIP: "+ul_penet_ytd_pol_cnt+" % , par: "+par_penet_ytd_pol_cnt+" % , Non-par: "+nonpar_penet_ytd_pol_cnt+" % & protection: "+protec_penet_ytd_pol_cnt+" %";	
 			}
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
