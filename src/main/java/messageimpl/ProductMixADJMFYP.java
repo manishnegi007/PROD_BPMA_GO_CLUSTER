f1991fff20486eb19f07736673e2a7477121c29f
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-11 19:08:17 +0530

    Update ProductMixADJMFYP.java

--- src/main/java/messageimpl/ProductMixADJMFYP.java
+++ src/main/java/messageimpl/ProductMixADJMFYP.java
@@ -47,7 +47,7 @@ public class ProductMixADJMFYP {
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
-			finalresponse="As of "+real_tim_timstamp+" MTD product mix ratio on Adj MFYP for " +user_region+ " region is ULIP: "+ul_penet_mtd_adj_mfyp+"% , Par: "+par_penet_mtd_adj_mfyp+"% & Non-Par: "+nonpar_penet_mtd_adj_mfyp+"% , protection: "+
+			finalresponse="As of "+real_tim_timstamp+" MTD product mix ratio on Adj MFYP for " +user_region+ " is ULIP: "+ul_penet_mtd_adj_mfyp+"% , Par: "+par_penet_mtd_adj_mfyp+"% & Non-Par: "+nonpar_penet_mtd_adj_mfyp+"% , protection: "+
 					protec_penet_mtd_adj_mfyp+ "%. YTD product mix ratio on Adj MFYP for " +user_region+ " is ULIP: "+ul_penet_ytd_adj_mfyp+"% , Par: "+par_penet_ytd_adj_mfyp+"% & Non-Par: "+nonpar_penet_ytd_adj_mfyp+"% , protection: "+
 					protec_penet_ytd_adj_mfyp+"%. If you want to see the channel wise business numbers, please specify.";	
 		}
