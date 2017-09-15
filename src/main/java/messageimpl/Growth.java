808821594698c2d403cb2697e230cfed7f67c238
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-15 12:12:18 +0530

    Update Growth.java

--- src/main/java/messageimpl/Growth.java
+++ src/main/java/messageimpl/Growth.java
@@ -11,19 +11,19 @@ public class Growth
 		{
 			finalresponse = msgChannel+" has witnessed paid Business growth of "+grth_ovr_lst_yr_paid
 					+"% on YTD basis, \n\n last year we had clocked "+adj_mfyp_sam_ytd_lst_yr+
-					"Cr of paid business, as compared to "+ytd_inforced_adj_mfyp+"Cr today";
+					"Cr of paid business, as compared to "+ytd_inforced_adj_mfyp+" Cr today";
 		}
 		else if("YTD".equalsIgnoreCase(period))
 		{
 			finalresponse = msgChannel+" has witnessed paid Business growth of "+grth_ovr_lst_yr_paid
 					+"% on YTD basis, \n\n last year we had clocked "+adj_mfyp_sam_ytd_lst_yr+
-					"Cr of Adj MFYP as compared to "+ytd_inforced_adj_mfyp+" today";
+					"Cr of Adj MFYP as compared to "+ytd_inforced_adj_mfyp+" Cr today";
 		}
 		else
 		{
 			finalresponse = msgChannel+" has witnessed paid Business growth of "+grth_lst_yr_sm_adj_mfyp_mtd
 					+"% on MTD basis, \n\n last year same month we had clocked "+prev_year_adj_mfyp_mtd+
-					"Cr of Adj MFYP as compared to "+mtd_inforced_adj_mfyp+" today";
+					"Cr of Adj MFYP as compared to "+mtd_inforced_adj_mfyp+" Cr today";
 			
 		}
 		return finalresponse.toString();
