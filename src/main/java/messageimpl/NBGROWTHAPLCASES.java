964bf458a8e4a285d4fcf9771323de5b16b2c72d
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2017-09-20 17:34:12 +0530

    Update NBGROWTHAPLCASES.java

--- src/main/java/messageimpl/NBGROWTHAPLCASES.java
+++ src/main/java/messageimpl/NBGROWTHAPLCASES.java
@@ -27,33 +27,33 @@ public class NBGROWTHAPLCASES
 		if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= "MLI has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
-					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
+					prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today MTD business Growth of "+ 
 					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
-					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " today."
 					+ " If you want to see the Channel wise business numbers, please specIfy the same.";
 		}
 		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= channel+" has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
-					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
+					prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today MTD business Growth of "+ 
 					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
-					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " today."
 					+ " If you want to see the Zone/region wise business numbers, please specIfy the same.";
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= "Zone "+ userzone+" "+channel+" has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
-					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
+					prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today MTD business Growth of "+ 
 					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
-					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " today."
 					+ " If you want to see region wise business numbers, please specIfy the same.";
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
 		{
 			finalresponse= "Region "+ user_region+" "+channel+ " has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
-					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
+					prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today MTD business Growth of "+ 
 					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
-					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " today."
 					+ " If you want to see the Zone/region wise business numbers, please specIfy the same.";					            
 		}
 		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
