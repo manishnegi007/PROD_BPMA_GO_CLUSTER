5c3a0a7a89806687cce43fa91a4da1930bf3bb10
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2018-01-12 15:36:53 +0530

    Update NBCaseSize.java

--- src/main/java/messageimpl/NBCaseSize.java
+++ src/main/java/messageimpl/NBCaseSize.java
@@ -3,7 +3,8 @@ package messageimpl;
 public class NBCaseSize 
 {
 	public static String nbCaseSizeIntent(String channel, String period, String user_circle, String user_region, String userzone, 
-			String real_tim_timstamp, String case_size_afyp_mtd, String case_size_afyp_ytd, String subchannel)
+			String real_tim_timstamp, String case_size_afyp_mtd, String case_size_afyp_ytd, String subchannel,
+			String user_clusters, String user_go)
 	{
 		String finalresponse="";
 
@@ -13,88 +14,125 @@ public class NBCaseSize
 		{period="";}
 		else
 		{
-			period=period.toUpperCase();
+			if("FTD".equalsIgnoreCase(period))
+			{
+				period="MTD";
+			}
+			else
+			{
+				period=period.toUpperCase();
+			}
 		}
 		if(!"".equalsIgnoreCase(user_circle))
 		{
 			user_region="Circle "+user_circle;
 		}
+		if(!"".equalsIgnoreCase(user_go))
+		{
+			user_clusters="Go "+user_go;
+		}
 		if(!"".equalsIgnoreCase(subchannel))
 	        {
                 channel = subchannel;
 	         }
-		if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+		if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
-			finalresponse= "As of " +real_tim_timstamp+ " Case Size MTD for MLI is Rs. "
-					+ case_size_afyp_mtd+ ". Case Size YTD for MLI is Rs. " +case_size_afyp_ytd+ 
+			finalresponse= "As of " +real_tim_timstamp+ " Case Size MTD for MLI is "
+					+ case_size_afyp_mtd+ " Case Size YTD for MLI is " +case_size_afyp_ytd+ 
 					". If you want to see the channel wise business numbers, please specify.";
-		}else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+		}else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
-			finalresponse= "As of " +real_tim_timstamp+ " Case Size MTD for "+channel+" is Rs. "+case_size_afyp_mtd+
-					". Case Size YTD for "+channel+" is Rs. "+case_size_afyp_ytd+
+			finalresponse= "As of " +real_tim_timstamp+ " Case Size MTD for "+channel+" is "+case_size_afyp_mtd+
+					" Case Size YTD for "+channel+" is "+case_size_afyp_ytd+
 					". If you want to see the zone/region wise business numbers, please specify.";
-		}else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+		}else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
-			finalresponse="As of " +real_tim_timstamp+" Case Size MTD for "+userzone+ " zone is Rs. "+ case_size_afyp_mtd+". Case Size YTD for "+
-					userzone+" zone is Rs. "+case_size_afyp_ytd+". If you want to see the region wise business numbers, please specify.";
+			finalresponse="As of " +real_tim_timstamp+" Case Size MTD for "+userzone+ " zone is "+ case_size_afyp_mtd+" Case Size YTD for "+
+					userzone+" zone is "+case_size_afyp_ytd+". If you want to see the region wise business numbers, please specify.";
 
-		}else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+		}else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
-			finalresponse="As of " +real_tim_timstamp+ " Case Size MTD for "+user_region+" is Rs. "+case_size_afyp_mtd+
-					". Case Size YTD for"+user_region+" is Rs. "+case_size_afyp_ytd;
-		}else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+			finalresponse="As of " +real_tim_timstamp+ " Case Size MTD for "+user_region+" zone is "+case_size_afyp_mtd+
+					" Case Size YTD for"+user_region+" is "+case_size_afyp_ytd;
+		}
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& !"".equalsIgnoreCase(user_clusters))
+		{
+			finalresponse="As of " +real_tim_timstamp+ " Case Size MTD for "+user_clusters+" zone is "+case_size_afyp_mtd+
+					" Case Size YTD for"+user_clusters+" zone is "+case_size_afyp_ytd;
+		}
+		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
-				finalresponse="As of " +real_tim_timstamp+ " Case Size " +period+" for "+channel+ " is Rs. "+ case_size_afyp_ytd+
+				finalresponse="As of " +real_tim_timstamp+ " Case Size " +period+" for "+channel+ " is "+ case_size_afyp_ytd+
 						". If you want to see the zone/region wise business numbers, please specify.";
 			}else 
 			{
-				finalresponse="As of " +real_tim_timstamp+ " Case Size " +period+" for "+channel+ " is Rs. "+ case_size_afyp_mtd+
+				finalresponse="As of " +real_tim_timstamp+ " Case Size " +period+" for "+channel+ " is "+ case_size_afyp_mtd+
 						". If you want to see the zone/region wise business numbers, please specify.";
 			}
 		}
-		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
-				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+userzone+" zone is Rs. "+case_size_afyp_ytd+
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+userzone+" zone is "+case_size_afyp_ytd+
 						". If you want to see the region wise business numbers, please specify.";
 			}else
 			{
-				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+userzone+" zone is Rs. "+case_size_afyp_mtd+
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+userzone+" zone is "+case_size_afyp_mtd+
 						". If you want to see the region wise business numbers, please specify.";
 			}
 		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
-				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_region+" is Rs. "+case_size_afyp_ytd;
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_region+" is "+case_size_afyp_ytd;
 			}else
 			{
-				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_region+" is Rs. "+case_size_afyp_mtd;
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_region+" is "+case_size_afyp_mtd;
 			}
 		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& !"".equalsIgnoreCase(user_clusters))
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
-				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_region+" is Rs. "+case_size_afyp_ytd;
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_clusters+" is "+case_size_afyp_ytd;
 			}else
 			{
-				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_region+" is Rs. "+case_size_afyp_mtd;
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_clusters+" is "+case_size_afyp_mtd;
+			}
+		}
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
+		{
+			if("YTD".equalsIgnoreCase(period))
+			{
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_region+" is "+case_size_afyp_ytd;
+			}else
+			{
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_region+" is "+case_size_afyp_mtd;
+			}
+		}
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& !"".equalsIgnoreCase(user_clusters))
+		{
+			if("YTD".equalsIgnoreCase(period))
+			{
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_clusters+" is "+case_size_afyp_ytd;
+			}else
+			{
+				finalresponse="As of " +real_tim_timstamp+" Case Size "+period+" for "+user_clusters+" is "+case_size_afyp_mtd;
 			}
 		}
 		else
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
-				finalresponse="As of " +real_tim_timstamp+"  Case Size "+period+ " for MLI is Rs. "+case_size_afyp_ytd+
+				finalresponse="As of " +real_tim_timstamp+"  Case Size "+period+ " for MLI is "+case_size_afyp_ytd+
 						". If you want to see the channel wise business numbers, please specify.";
 			}else 
 			{
-				finalresponse="As of " +real_tim_timstamp+"  Case Size "+period+ " for MLI is Rs. "+case_size_afyp_mtd+
+				finalresponse="As of " +real_tim_timstamp+"  Case Size "+period+ " for MLI is "+case_size_afyp_mtd+
 						". If you want to see the channel wise business numbers, please specify.";
 			}
 		}
