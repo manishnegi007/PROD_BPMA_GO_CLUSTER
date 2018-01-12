03605c01dd1ee42e35729546683ca4a372910538
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2018-01-12 15:56:47 +0530

    Update NBGROWTHAPLCASES.java

--- src/main/java/messageimpl/NBGROWTHAPLCASES.java
+++ src/main/java/messageimpl/NBGROWTHAPLCASES.java
@@ -4,7 +4,8 @@ public class NBGROWTHAPLCASES
 {
 	public static String nbGROWTHAPLCASESIntent(String channel, String period, String user_circle, String user_region,
 			String userzone, String prev_applied_cases_ytd, String grth_applied_cases_ytd, String grth_applied_cases_mtd, 
-			String applied_cases_ytd, String prev_applied_cases_mtd, String applied_cases_mtd, String subchannel)
+			String applied_cases_ytd, String prev_applied_cases_mtd, String applied_cases_mtd, String subchannel,
+			String user_clusters, String user_go)
 	{
 		String finalresponse="";
 
@@ -14,49 +15,68 @@ public class NBGROWTHAPLCASES
 		{period="";}
 		else
 		{
-			period=period.toUpperCase();
+			if("FTD".equalsIgnoreCase(period))
+			{
+				period="MTD";
+			}
+			else
+			{
+				period=period.toUpperCase();
+			}
 		}
 		if(!"".equalsIgnoreCase(user_circle))
 		{
 			user_region="Circle "+user_circle;
 		}
+		if(!"".equalsIgnoreCase(user_go))
+		{
+			user_clusters="Go "+user_go;
+		}
 		if(!"".equalsIgnoreCase(subchannel))
 	        {
                 channel = subchannel;
 	         }
-		if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+		if("".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			finalresponse= "MLI has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
-					prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today MTD business Growth of "+ 
+					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
 					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
-					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " today."
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
 					+ " If you want to see the Channel wise business numbers, please specIfy the same.";
 		}
-		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			finalresponse= channel+" has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
-					prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today MTD business Growth of "+ 
+					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
 					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
-					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " today."
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
 					+ " If you want to see the Zone/region wise business numbers, please specIfy the same.";
 		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			finalresponse= "Zone "+ userzone+" "+channel+" has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
-					prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today MTD business Growth of "+ 
+					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
 					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
-					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " today."
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
 					+ " If you want to see region wise business numbers, please specIfy the same.";
 		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			finalresponse= "Region "+ user_region+" "+channel+ " has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
-					prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today MTD business Growth of "+ 
+					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
 					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
-					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " today."
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
 					+ " If you want to see the Zone/region wise business numbers, please specIfy the same.";					            
 		}
-		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && "".equalsIgnoreCase(period)&& !"".equalsIgnoreCase(user_clusters))
+		{
+			finalresponse= "Cluster "+ user_clusters+" "+channel+ " has witnessed applied Business growth of "+grth_applied_cases_ytd+" % on YTD basis, last year same time we had clocked "+
+					prev_applied_cases_ytd+ " Cr of Applied Cases as compared to " +applied_cases_ytd+ " Cr today MTD business Growth of "+ 
+					grth_applied_cases_mtd+ " % on MTD basis, last year same month we have clocked "+
+					prev_applied_cases_mtd+" of Applied Cases as compared to " +applied_cases_mtd+ " Cr today."
+					+ " If you want to see the Zone/region wise business numbers, please specIfy the same.";					            
+		}
+		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
@@ -71,7 +91,7 @@ public class NBGROWTHAPLCASES
 						+ ". If you want to see the Zone/region wise business numbers, please specIfy the same.";	
 			}
 		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
@@ -86,7 +106,7 @@ public class NBGROWTHAPLCASES
 						+ ". If you want to see the Zone/region wise business numbers, please specIfy the same.";	
 			}
 		}
-		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
@@ -101,7 +121,22 @@ public class NBGROWTHAPLCASES
 						+ ". If you want to see the Zone/region wise business numbers, please specIfy the same.";	
 			}
 		}
-		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period))
+		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& !"".equalsIgnoreCase(user_clusters))
+		{
+			if("YTD".equalsIgnoreCase(period))
+			{
+				finalresponse= "Cluster " +user_clusters+" "+channel+" has witnessed applied Business growth of " +grth_applied_cases_ytd+" % on "+period+" basis, last year same time we had clocked "+
+						prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today "
+						+ ". If you want to see the Zone/region wise business numbers, please specIfy the same.";
+			}
+			else
+			{
+				finalresponse= "Cluster " +user_clusters+" "+channel+" has witnessed applied Business growth of " +grth_applied_cases_mtd+" % on "+period+" basis, last year same time we had clocked "+
+						prev_applied_cases_mtd+ " of Applied Cases as compared to " +applied_cases_mtd+ " today "
+						+ ". If you want to see the Zone/region wise business numbers, please specIfy the same.";	
+			}
+		}
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& "".equalsIgnoreCase(user_clusters))
 		{
 			if("YTD".equalsIgnoreCase(period))
 			{
@@ -112,6 +147,21 @@ public class NBGROWTHAPLCASES
 			else
 			{
 				finalresponse= "Region " +user_region+" "+channel+" has witnessed applied Business growth of " +grth_applied_cases_mtd+" % on "+period+" basis, last year same time we had clocked "+
+						prev_applied_cases_mtd+ " of Applied Cases as compared to " +applied_cases_mtd+ " today "
+						+ ". If you want to see the Zone/region wise business numbers, please specIfy the same.";	
+			}
+		}
+		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) && !"".equalsIgnoreCase(period)&& !"".equalsIgnoreCase(user_clusters))
+		{
+			if("YTD".equalsIgnoreCase(period))
+			{
+				finalresponse= "Cluster " +user_clusters+" "+channel+" has witnessed applied Business growth of " +grth_applied_cases_ytd+" % on "+period+" basis, last year same time we had clocked "+
+						prev_applied_cases_ytd+ " of Applied Cases as compared to " +applied_cases_ytd+ " today "
+						+ ". If you want to see the Zone/region wise business numbers, please specIfy the same.";
+			}
+			else
+			{
+				finalresponse= "Cluster " +user_clusters+" "+channel+" has witnessed applied Business growth of " +grth_applied_cases_mtd+" % on "+period+" basis, last year same time we had clocked "+
 						prev_applied_cases_mtd+ " of Applied Cases as compared to " +applied_cases_mtd+ " today "
 						+ ". If you want to see the Zone/region wise business numbers, please specIfy the same.";	
 			}
