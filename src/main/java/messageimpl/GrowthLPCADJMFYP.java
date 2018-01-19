71e850faefc279f17b66326e53ba98ba39c658a2
priyanka897 <priyanka.mittal@maxlifeinsurance.com>
2018-01-19 18:39:41 +0530

    Update GrowthLPCADJMFYP.java

--- src/main/java/messageimpl/GrowthLPCADJMFYP.java
+++ src/main/java/messageimpl/GrowthLPCADJMFYP.java
@@ -77,8 +77,8 @@ public class GrowthLPCADJMFYP {
 			finalresponse= ""+user_clusters+" has witnessed paid business growth of "+grth_lpc_paid_adj_mfyp_ytd+"% on YTD basis, last year same time we had clocked "+
 					prev_lpc_paid_adj_mfyp_ytd+ "Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_ytd_growth+ "Cr today MTD business Growth of "+ 
 					grth_lpc_paid_adj_mfyp_mtd+ "% on MTD basis, last year same month we have clocked "+
-					prev_lpc_paid_adj_mfyp_mtd+"Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_mtd_growth+ "Cr today."
-					+ ".";
+					prev_lpc_paid_adj_mfyp_mtd+"Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_mtd_growth+ "Cr today.";
+					
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region)
 			&& !"".equalsIgnoreCase(user_clusters) && "".equalsIgnoreCase(period))
@@ -86,8 +86,8 @@ public class GrowthLPCADJMFYP {
 			finalresponse= ""+user_clusters+" has witnessed paid business growth of "+grth_lpc_paid_adj_mfyp_ytd+"% on YTD basis, last year same time we had clocked "+
 					prev_lpc_paid_adj_mfyp_ytd+ "Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_ytd_growth+ "Cr today MTD business Growth of "+ 
 					grth_lpc_paid_adj_mfyp_mtd+ "% on MTD basis, last year same month we have clocked "+
-					prev_lpc_paid_adj_mfyp_mtd+"Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_mtd_growth+ "Cr today."
-					+ ".";
+					prev_lpc_paid_adj_mfyp_mtd+"Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_mtd_growth+ "Cr today.";
+					
 		}
 		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && "".equalsIgnoreCase(user_region) 
 			&& !"".equalsIgnoreCase(user_clusters) && "".equalsIgnoreCase(period))
@@ -95,8 +95,8 @@ public class GrowthLPCADJMFYP {
 			finalresponse= ""+user_clusters+" has witnessed paid business growth of "+grth_lpc_paid_adj_mfyp_ytd+"% on YTD basis, last year same time we had clocked "+
 					prev_lpc_paid_adj_mfyp_ytd+ "Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_ytd_growth+ "Cr today MTD business Growth of "+ 
 					grth_lpc_paid_adj_mfyp_mtd+ "% on MTD basis, last year same month we have clocked "+
-					prev_lpc_paid_adj_mfyp_mtd+"Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_mtd_growth+ "Cr today."
-					+ ".";
+					prev_lpc_paid_adj_mfyp_mtd+"Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_mtd_growth+ "Cr today.";
+					
 		}
 		else if(!"".equalsIgnoreCase(channel) && "".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) 
 			&& "".equalsIgnoreCase(user_clusters) && "".equalsIgnoreCase(period))
@@ -161,14 +161,14 @@ public class GrowthLPCADJMFYP {
 			if("YTD".equalsIgnoreCase(period))
 			{
 				finalresponse= ""+user_clusters+" has witnessed paid business growth of " +grth_lpc_paid_adj_mfyp_ytd+" % on "+period+" basis, last year same time we had clocked "+
-						prev_lpc_paid_adj_mfyp_ytd+ "Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_ytd_growth+ "Cr today "
-						+ ".";
+						prev_lpc_paid_adj_mfyp_ytd+ "Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_ytd_growth+ "Cr today. ";
+						
 			}
 			else
 			{
 				finalresponse= ""+user_clusters+" has witnessed paid business growth of " +grth_lpc_paid_adj_mfyp_mtd+" % on "+period+" basis, last year same time we had clocked "+
-						prev_lpc_paid_adj_mfyp_mtd+ "Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_mtd_growth+ "Cr today "
-						+ ".";	
+						prev_lpc_paid_adj_mfyp_mtd+ "Cr of LPC paid Adj MFYP as compared to " +lpc_paid_adj_mfyp_mtd_growth+ "Cr today. ";
+						
 			}
 		}
 		else if(!"".equalsIgnoreCase(channel) && !"".equalsIgnoreCase(userzone) && !"".equalsIgnoreCase(user_region) 
